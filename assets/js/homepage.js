017094a6bba3ddf89becd65c8b260fb7fd5405b2
Symphony <55611595+lnance6@users.noreply.github.com>
2020-09-12 17:55:17 -0700

    added issues list

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -29,14 +29,16 @@ var displayRepos = function(repos, searchTerm) {
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
   // loop over repos
+  // loop over repos
   for (var i = 0; i < repos.length; i++) {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
-
+    var repo = repoName.value;
     // create a container for each repo
-    var repoEl = document.createElement("div");
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
-
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+    
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
--- assets/js/single.js
+++ assets/js/single.js
@@ -1,21 +1,47 @@
+var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+};
+
 var getRepoIssues = function(repo) {
     console.log(repo);
-    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc&" ;
 
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
-          response.json().then(function(data) {
-            // pass response data to dom function
-            displayIssues(data);
-          });
+            response.json().then(function(data) {
+                displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            });
+        } else {
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
-        else {
-          alert("There was a problem with your request!");
-        }
-      });
-  };
+    });
+
+    
+};
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -52,4 +78,19 @@ var displayIssues = function(issues) {
     }
 };
 
- getRepoIssues("mslnance/mslnance-taskmaster-pro");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+
+getRepoName();
+// getRepoIssues(repoName);
+ 
