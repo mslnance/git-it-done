2406df6bfa064485ae32a0671e0a0701e59fb126
Symphony <55611595+lnance6@users.noreply.github.com>
2020-09-12 19:01:49 -0700

    added language selection

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,22 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+
+var getFeaturedRepos= function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
 
 var formSubmitHandler = function(event) {
   event.preventDefault();
@@ -87,5 +103,19 @@ var getUserRepos = function(user) {
       alert("Unable to connect to GitHub");
   });
 };
-  
+
+
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
+  
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+};
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
